afe3cd40325d4972759248cb2f71cffd61645663
ymyoon(OMEN) <ozone753@naver.com>
2023-10-01 23:58:17 +0900

    캐릭터가 클릭된 좌표로 이동추가_임시

--- move_to_cursor.py
+++ move_to_cursor.py
@@ -29,19 +29,42 @@ def cursor_point():
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
+frame2 = 0
 cursor_x, cursor_y = 0, 0
 points = []
 
 def char_move_to_cursor():
-    pass
+    global frame, frame2
+    global x, y
+
+    if len(points) > 0:  
+        cursor_x, cursor_y = points[0]
+    else:
+        cursor_x, cursor_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+
+    t = 1 / 100
+    x = (1 - t) * x + t * cursor_x
+    y = (1 - t) * y + t * cursor_y
+
+    if x > cursor_x:  # 왼쪽으로 이동할 때 char.clip_draw 함수 내의 y좌표 조정
+            frame2 = 0
+    else:
+            frame2 = 1
+
+    char.clip_draw(frame * 100, frame2 * 100, 100, 100, x, y)
+    frame = (frame + 1) % 8
+    frame2 = (frame2 + 1) % 8
+    delay(0.05)
+
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    char.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    char_move_to_cursor()
     cursor_point()
     update_canvas()
     frame = (frame + 1) % 8
+    frame2 = (frame2 + 1) % 8
     handle_events()
 
 close_canvas()
